666964a0e6d5b10f98e4e27bbd43c433af1c0950
andrew18920 <43442534+andrew18920@users.noreply.github.com>
2019-10-16 15:34:50 +0100

    Sped it up a bit
    fetches matches concurrently and gets players names without an extra request

--- ex_player_finder.py
+++ ex_player_finder.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from multiprocessing.dummy import Pool
 from functools import partial
-import datetime
+from datetime import datetime, timedelta
 
 head = {
     'Host':'www.transfermarkt.co.uk',
@@ -16,15 +16,27 @@ head = {
     'TE': 'Trailers'
 }
 
+def is_time_format(input):
+    try:
+        datetime.strptime(input, '%I:%M %p')
+        return True
+    except ValueError:
+        return False
+
 def used_to_play_for(team, player):
     if player=='':
         return False
     html = requests.get('https://www.transfermarkt.co.uk/player-name/transfers/spieler/{}'.format(player if player.isnumeric() else find_player(player)), headers=head)
     soup = BeautifulSoup(html.text, features='html.parser')
     try:
-        return team.lower() in soup.find_all('tr', class_='zeile-transfer')[0].parent.get_text().lower()
+        ret = team.lower() in soup.find_all('tr', class_='zeile-transfer')[0].parent.get_text().lower()
     except:
-        return False
+        ret = False
+
+    player_name = ''
+    if ret:
+        player_name = soup.title.text.replace(' - Transfer history | Transfermarkt','')
+    return (player_name, ret)
 
 def find_player(name):
     html = requests.get('https://www.transfermarkt.co.uk/schnellsuche/ergebnis/schnellsuche?query={}'.format(name), headers=head)
@@ -38,11 +50,6 @@ def get_players_involved(match):
     subs = [plr['id'] for plr in soup.find_all('td', class_='aufstellung-rueckennummer-ersatzbank')]
     return starters+subs
 
-def get_player_name(player):
-    html = requests.get('https://www.transfermarkt.co.uk/player-name/transfers/spieler/{}'.format(player), headers=head)
-    soup = BeautifulSoup(html.text, features='html.parser')
-    return soup.title.text.replace(' - Transfer history | Transfermarkt','')
-
 def any_ex_players(team, match):
     plrz = get_players_involved(match)
     pool = Pool(4)
@@ -51,28 +58,27 @@ def any_ex_players(team, match):
     players = []
     
     for x in range(0, len(plrz)):
-        if playersTF[x]:
-            players.append(get_player_name(plrz[x]))
+        if playersTF[x][1]:
+            players.append(playersTF[x][0])
     return players
 
-#def any_ex_players(team, match):
-#    plrz = get_players_involved(match)
-#    players = []
-#    for player in plrz:
-#        if used_to_play_for(team, player):
-#            players.append(get_player_name(player))
-#    return players
-
 def fetch_current_matches():
-    # $('.begegnungZeile')
     html = requests.get('https://www.transfermarkt.co.uk/ticker/index/live', headers=head)
     soup = BeautifulSoup(html.text, features='html.parser')
     home_tds = soup.find_all('td', class_='verein-heim')
     away_tds = soup.find_all('td', class_='verein-gast')
-    matches = [tr['id'] for tr in soup.find_all('tr', class_='begegnungZeile')]
+    all_matches = [(tr['id'], tr.find_all('span', class_='matchresult')[0].text) for tr in soup.find_all('tr', class_='begegnungZeile')]
+    matches = []
+    for match in all_matches:
+        if is_time_format(match[1]):
+            kick_off = datetime.strptime(match[1], '%I:%M %p')
+            if datetime.now().time() >= (kick_off-timedelta(hours=1)).time():
+                matches.append(match[0])
+        else:
+            matches.append(match[0])
     home = [td.find_all('a', class_='vereinprofil_tooltip')[0].text for td in home_tds]
     away = [td.find_all('a', class_='vereinprofil_tooltip')[1].text for td in away_tds]
-
+    
     return list(zip(matches,(zip(home, away))))
 
 def print_current_matches(matches):
@@ -82,20 +88,30 @@ def print_current_matches(matches):
         print('{}. {} v {}'.format(i, match[1][0], match[1][1]))
     return matches
 
+def check_match(match):
+    all_ex_players = []
+    ex_players = any_ex_players(team, match[0])
+    if len(ex_players)>0:
+        all_ex_players.append(('{} v {}: '.format(match[1][0], match[1][1]), ex_players))
+
+    print('-', end='')
+    return all_ex_players
+
 matches = print_current_matches(fetch_current_matches())
-choice = input('\nEnter match choice: (1-{} or type \'all\' for all matches)\n> ').format(len(matches))
+choice = input('\nEnter match choice: (1-{} or type \'all\' for all matches)\n> '.format(len(matches)))
 team = input('\nEnter a team: \n> ')
 
+start_time = datetime.now()
 if choice=='all':
-    # Could make this bit concurrent/threaded
-    all_ex_players = []
-    for match in matches:
-        ex_players = any_ex_players(team, match[0])
-        if len(ex_players)>0:
-            all_ex_players.append(('{} v {}: '.format(match[1][0], match[1][1]), ex_players))
+    pool = Pool(4)
+    all_ex_players = [x[0] for x in pool.map(check_match, matches) if len(x)>0]
 
     print('\n')
+    total_players = 0
     for game in all_ex_players:
-        print('{}{}'.format(game[0], ', '.join(game[1])))
+        total_players += len(game[1])
+        print('\n{}{}'.format(game[0], ', '.join(game[1])))
+    print('\nFound {} players in {}'.format(total_players, datetime.now() - start_time))
 else:
-    print('\n'+str(any_ex_players(team, matches[int(choice)-1][0])))
+    ex_players = any_ex_players(team, matches[int(choice)-1][0])
+    print('\n'+str(ex_players)+'\nFound {} players in {}'.format(len(ex_players), datetime.now() - start_time))
